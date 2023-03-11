e48f498533d0b1b56c23ad0be06ba227ee7d8ce9
unknown <1772964666@qq.com>
2023-03-11 22:00:39 +0800

    提交

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { ref, reactive } from 'vue'
+import { useRoute } from 'vue-router'
+import IndexHeader from './components/global/indexHeader.vue';
+const route = useRoute();
+
+</script>
+
+<template>
+  <el-container>
+    <el-header height="40px">
+      <IndexHeader v-if="route.meta.needHeader"></IndexHeader>
+    </el-header>
+    <el-main>
+      <router-view></router-view>
+    </el-main>
+  </el-container>
+</template>
+
+<style scoped lang="scss">
+@import "@/assets/sass/global/indexHeader.scss";
+</style>
